41b554240fb26ccabee734736704d394ed715bac
JoVaKnee <43181583+JoVaKnee@users.noreply.github.com>
2018-09-18 17:14:03 +0100

    Create Hellocazzo.py

---
+++ giovi Lollotown stuff   - Chapter 1 UnderChapter 1  - Chapter 2 UnderChapter2  - Chapter 3 UnderChapter 2  - links link to -Gitlab-htpp/Hellocazzo.py
@@ -0,0 +1,19 @@
+# giovi
+Lollotown stuff
+
+
+# Chapter 1
+UnderChapter 1
+
+## Chapter 2
+UnderChapter2
+
+## Chapter 3
+UnderChapter 2
+
+# links
+link to [Gitlab](htpp://gitlab.com) like this
+
+```py
+print('cazzofigaporcodio')
+```
